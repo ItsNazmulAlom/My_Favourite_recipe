d8afe6d3797298f3e4b6292b961bd3cacdfc2161
Md.Nazmul Alom <34202639+CoderNazmulAlom@users.noreply.github.com>
2019-11-24 00:30:13 +0600

    Initial commit

---
+++ app/src/main/java/com/nazmul/my_favourite_recipe/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nazmul.my_favourite_recipe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
