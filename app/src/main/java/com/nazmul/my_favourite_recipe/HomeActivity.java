d9b9441472d098524da5f5cddc85de711dabf074
Md.Nazmul Alom <34202639+CoderNazmulAlom@users.noreply.github.com>
2019-11-24 02:02:36 +0600

    splash activity

--- app/src/main/java/com/nazmul/my_favourite_recipe/HomeActivity.java
+++ app/src/main/java/com/nazmul/my_favourite_recipe/HomeActivity.java
@@ -1,32 +1,41 @@
 package com.nazmul.my_favourite_recipe;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        getSupportActionBar().setHomeButtonEnabled(true); //for back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//for back button
+        getSupportActionBar().setTitle("Details");//for actionbar title
+
+
+
+
     }
 
+
+
+
+    //for back button
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
--- app/src/main/java/com/nazmul/my_favourite_recipe/SplashActivity.java
+++ app/src/main/java/com/nazmul/my_favourite_recipe/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.nazmul.my_favourite_recipe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,5 +15,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar= getSupportActionBar();
+        actionBar.hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },splashTimeOut);
     }
 }
